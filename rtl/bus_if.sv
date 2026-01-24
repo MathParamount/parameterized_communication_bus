9c3ac9e102dbf840faba1118140887fa061722a7
Gabriel da cruz guedes santos <154000020+MathParamount@users.noreply.github.com>
2026-01-24 16:12:40 -0300

    Implement master_write and master_read tasks
    Adding tasks to diminish the testbench code and improve creation of stimulus

--- rtl/bus_if.sv
+++ rtl/bus_if.sv
@@ -19,4 +19,37 @@ interface bus_if #(DATA_WIDTH = 32, ADDR_WIDTH = 16);
 
 	modport slave_reg (input addr,write_data,write,valid,ready, output read,read_data);
 
+
+	//To create stimulus to testbench encapsulation
+	task master_write(input logic [ADDR_WIDTH-1:0] addr, input logic [DATA_WIDTH-1:0] data);
+
+		@(posedge clk);
+		valid <= 1;
+		read <= 0;
+		write <= 1;
+		
+		this.addr <= addr;
+	
+		write_data <= data;
+		
+		wait(ready);
+		valid <= 0;
+	
+	endtask
+
+	task master_read(input logic [ADDR_WIDTH-1:0] addr);
+		
+		@(posedge clk);
+		valid <= 1;
+		read <= 1;
+		write <= 0;
+
+		this.addr <= addr;
+		wait(ready);
+
+		$display("The data is: %h", read_data);
+		valid <= 0;
+
+	endtask
+	
 endinterface : bus_if
